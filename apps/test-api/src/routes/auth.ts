cae2c60bd751c5f7e83c593a5934a448e18b632f
MC <55539221+sarisssa@users.noreply.github.com>
2025-07-18 11:49:21 -0700

    feat: implement twilio endpoints for send otp + verify otp

--- apps/test-api/src/routes/auth.ts
+++ apps/test-api/src/routes/auth.ts
@@ -1,11 +1,77 @@
 import { FastifyInstance } from 'fastify';
+import twilio from 'twilio';
+import { SendOtpBody, VerifyOtpBody } from '../types/auth';
 
 export default async function authRoutes(fastify: FastifyInstance) {
-  fastify.post('/send-otp', async () => {
-    return { status: 'OTP endpoint' };
+  const client = twilio(
+    fastify.config.TWILIO_ACCOUNT_SID,
+    fastify.config.TWILIO_AUTH_TOKEN
+  );
+
+  fastify.post<{ Body: SendOtpBody }>('/send-otp', async (request, reply) => {
+    let { phoneNumber } = request.body;
+
+    //Server side safegaurd to ensure phone number is in E.164 format
+    if (!phoneNumber.startsWith('+')) {
+      phoneNumber = '+1' + phoneNumber.replace(/\D/g, '');
+    }
+
+    try {
+      const verification = await client.verify.v2
+        .services(fastify.config.TWILIO_VERIFY_SERVICE_SID)
+        .verifications.create({
+          channel: 'sms',
+          to: phoneNumber,
+        });
+
+      fastify.log.info({
+        verificationSid: verification.sid,
+        phoneNumber,
+        msg: 'OTP sent successfully',
+      });
+
+      reply.status(200).send({ message: 'OTP sent successfully' });
+    } catch (error) {
+      fastify.log.error({ error, msg: 'Error sending OTP' });
+      reply.status(500).send({
+        message: 'Failed to send OTP',
+        error: (error as Error).message,
+      });
+    }
   });
 
-  fastify.post('/logout', async () => {
-    return { status: 'Logout endpoint' };
-  });
+  fastify.post<{ Body: VerifyOtpBody }>(
+    '/verify-otp',
+    async (request, reply) => {
+      const { code } = request.body;
+      let { phoneNumber } = request.body;
+
+      if (!phoneNumber.startsWith('+')) {
+        phoneNumber = '+1' + phoneNumber.replace(/\D/g, '');
+      }
+
+      try {
+        const verificationCheck = await client.verify.v2
+          .services(fastify.config.TWILIO_VERIFY_SERVICE_SID)
+          .verificationChecks.create({
+            code,
+            to: phoneNumber,
+          });
+
+        fastify.log.info({
+          verificationSid: verificationCheck.sid,
+          phoneNumber,
+          msg: 'OTP verified successfully',
+        });
+
+        reply.status(200).send({ message: 'OTP verified successfully' });
+      } catch (error) {
+        fastify.log.error({ error, msg: 'Error sending OTP' });
+        reply.status(500).send({
+          message: 'Failed to verify OTP',
+          error: (error as Error).message,
+        });
+      }
+    }
+  );
 }
---
+++ apps/test-api/src/types/auth.ts
@@ -0,0 +1,25 @@
+import { z } from 'zod';
+
+export const sendOtpBodySchema = z.object({
+  phoneNumber: z
+    .string()
+    .min(10, 'Phone number must be at least 10 digits.')
+    .regex(
+      /^\+?\d{10,15}$/,
+      'Invalid phone number format. Must be E.164 format or a 10-15 digit number.'
+    ),
+});
+export type SendOtpBody = z.infer<typeof sendOtpBodySchema>;
+
+export const verifyOtpBodySchema = z.object({
+  code: z.string().length(6, 'OTP code must be exactly 6 digits.'),
+
+  phoneNumber: z
+    .string()
+    .min(10, 'Phone number must be at least 10 digits.')
+    .regex(
+      /^\+?\d{10,15}$/,
+      'Invalid phone number format. Must be E.164 format or a 10-15 digit number.'
+    ),
+});
+export type VerifyOtpBody = z.infer<typeof verifyOtpBodySchema>;
