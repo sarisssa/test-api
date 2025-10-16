b2991f56873449d2c05348aebbe1937c741e2de5
MC <55539221+sarisssa@users.noreply.github.com>
2025-10-15 17:05:36 -0700

    feat: remove asset manager

--- apps/asset-manager/src/api/health.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response } from "express";
-import { metrics } from "../services/metrics";
-
-export async function getHealth(req: Request, res: Response) {
-  const healthData = metrics.getHealthData();
-  res.json(healthData);
-} 
--- apps/asset-manager/src/api/metrics.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response } from "express";
-import { metrics } from "../services/metrics";
-
-export async function getMetrics(req: Request, res: Response) {
-  res.set('Content-Type', 'text/plain');
-  res.send(metrics.exportMetrics());
-} 
